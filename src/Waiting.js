9434449af47c6508345ec3309cd764e9c5cfb35a
815147-1115147 <jluiaa@connect.ust.hk>
2021-05-06 02:51:01 +0800

    .

--- src/Waiting.js
+++ src/Waiting.js
@@ -1,7 +1,6 @@
 /* eslint-disable quotes */
 import React, { useState, useEffect } from "react";
 import { SafeAreaView, Text, ScrollView, StyleSheet, View } from "react-native";
-<<<<<<< HEAD:src/components/Waiting.js
 import { Avatar, Button } from "react-native-elements";
 // import database from "@react-native-firebase/database";
 // import MMKVStorage from "react-native-mmkv-storage";
@@ -10,24 +9,9 @@ import { color } from "../constants.json";
 import { deleteGame, getGame } from "./Helper/server";
 import useInterval from "./Helper/useInterval";
 // import { wsSend } from "../App";
-=======
-import { Avatar, Button} from "react-native-elements";
-//import database from "@react-native-firebase/database";
-import MMKVStorage from "react-native-mmkv-storage";
-import AsyncStorage from "@react-native-async-storage/async-storage";
-
-import { color } from "../constants.json";
-//import { deleteGame, getGame } from "./Helper/server";
-//import useInterval from "./Helper/useInterval";
-import useInterval from "./useInterval";
-//import { Icon } from "react-native-elements/dist/icons/Icon";
->>>>>>> parent of 876f8ba (.):src/Waiting.js
 import Icon from "react-native-vector-icons";
-//import { wsSend } from "../App";
-AsyncStorage.clear();
 
 export default Waiting = ({ navigation }) => {
-<<<<<<< HEAD:src/components/Waiting.js
   // const MMKV = new MMKVStorage.Loader().initialize();
 
   // const [game, setGame] = useState(MMKV.getMap("joinedGame"));
@@ -35,88 +19,8 @@ export default Waiting = ({ navigation }) => {
   const [playerView, setPlayersView] = useState([]);
   // const gameID = MMKV.getString("gameID");
   // const userID = MMKV.getString("userID");
-=======
-  const MMKV = new MMKVStorage.Loader().initialize();
-  //const [game, setGame] = useState(MMKV.getMap("joinedGame"));
-  const [roomInfo, setRoomInfo] = useState(null);
-  const [playerView, setPlayersView] = useState([]);
-  //   const gameID = MMKV.getString("gameID");
-  //   const userID = MMKV.getString("userID");
-  const gameID = "gameID";
-  const userID = "userID";
->>>>>>> parent of 876f8ba (.):src/Waiting.js
   let status;
   let playerList = [];
-  
-  var Player = function() {
-    this.pid = pid;
-    this.name = name;
-  };
-
-  var game = {
-      gid: "Game ID",
-      gname: "gameName",
-      status: "PREPARE",
-      //hostID: MMKV.getString("userID"),
-      hostID: "userID",
-      //hostName: MMKV.getString("userName"),
-      hostName: "userName",
-      checkpoints: [
-          {
-              cp1: "cp1",
-              cp2: "cp2",
-              cp3: "cp3",
-              cp4: "cp4",
-              cp5: "cp5",
-          },
-        ],
-      players: [
-          {
-            //   pid: MMKV.getString("userID"),
-            //   name: MMKV.getString("userName"),
-            //   avatar: "None",
-            pid: "pid1",
-            name: "userName1",
-            avatar: "None1",
-          },
-          {
-            pid: "pid2",
-            name: "userName2",
-            avatar: "None2",
-          },
-          {
-            pid: "pid3",
-            name: "userName3",
-            avatar: "None3",
-          },
-        //   {
-        //     pid: "pid4",
-        //     name: "userName4",
-        //     avatar: "None4",
-        //   },
-        //   {
-        //     pid: "pid5",
-        //     name: "userName5",
-        //     avatar: "None5",
-        //   },
-        //   {
-        //     pid: "pid6",
-        //     name: "userName6",
-        //     avatar: "None6",
-        //   },
-        //   {
-        //     pid: "pid7",
-        //     name: "userName7",
-        //     avatar: "None7",
-        //   },
-        //   {
-        //     pid: "pid8",
-        //     name: "userName8",
-        //     avatar: "None8",
-        //   }
-        ],
-      teams: ["RED", "BLUE"],
-    };
 
   var game = {
     gid: "Game ID",
@@ -200,13 +104,8 @@ export default Waiting = ({ navigation }) => {
 
   useEffect(() => {
     let list = [];
-<<<<<<< HEAD:src/components/Waiting.js
     // game.players.map((value) => list.push(value.name));
     playerList = list;
-=======
-    //game.players.map((value) => list.push(value.name));
-    playerList = ["Player1","Player2","Player3","Player4","Player5","Player6","Player7","Player8","Player9","Player10"];
->>>>>>> parent of 876f8ba (.):src/Waiting.js
     setPlayersView(renderPlayersList());
   }, []);
 
@@ -215,11 +114,7 @@ export default Waiting = ({ navigation }) => {
     status = roomInfo.status;
     //let game = MMKV.getMap("joinedGame");
     game.players = roomInfo.players;
-<<<<<<< HEAD:src/components/Waiting.js
     let list = [];
-=======
-    //let list = [];
->>>>>>> parent of 876f8ba (.):src/Waiting.js
     // roomInfo.players.map((value) => {
     //   list.push(value.name);
     //   if (value.team != null) {
@@ -229,10 +124,7 @@ export default Waiting = ({ navigation }) => {
     //     }
     //   }
     // });
-<<<<<<< HEAD:src/components/Waiting.js
     playerList = list;
-=======
->>>>>>> parent of 876f8ba (.):src/Waiting.js
     setPlayersView(renderPlayersList());
     if (status === "RUNNING") {
       navigation.replace("InGame");
@@ -252,11 +144,11 @@ export default Waiting = ({ navigation }) => {
                 {playerList[i]}
             </Text>
             <Avatar
-                rounded
-                source={{
-                    uri:"https://images-na.ssl-images-amazon.com/images/S/pv-target-images/7bbe5762c79ee0ad11c1267483b4a2d5e12868de779eaf751e8e86596e978bbb._V_SX1080_.jpg",
-                }}
-                //icon={{name: 'home'}}
+              rounded
+              source={{
+                uri:"https://images-na.ssl-images-amazon.com/images/S/pv-target-images/7bbe5762c79ee0ad11c1267483b4a2d5e12868de779eaf751e8e86596e978bbb._V_SX1080_.jpg",
+              }}
+              //icon={{name: 'home'}}
             />
             {/* <Icon
                 reverse
@@ -268,22 +160,15 @@ export default Waiting = ({ navigation }) => {
           </View>
           <View style={styles.rightPlayer} key={i + 1}>
             <Avatar
-                rounded
-                
-                source={{
-                    uri:"https://images-na.ssl-images-amazon.com/images/S/pv-target-images/7bbe5762c79ee0ad11c1267483b4a2d5e12868de779eaf751e8e86596e978bbb._V_SX1080_.jpg",
-                }}
-                //icon={{name: 'home'}}
+              rounded
+              source={{
+                uri:"https://images-na.ssl-images-amazon.com/images/S/pv-target-images/7bbe5762c79ee0ad11c1267483b4a2d5e12868de779eaf751e8e86596e978bbb._V_SX1080_.jpg",
+              }}
+              //icon={{name: 'home'}}
             />
-            <Text style={styles.RplayerName} key={i+1}>
-                {playerList[i+1]}
+            <Text style={styles.RplayerName} key={i + 1}>
+              {playerList[i + 1]}
             </Text>
-            {/* <Icon
-                //reverse
-                name="rocket"
-                size={20}
-                //color="white"
-            /> */}
           </View>
         </View>
       );
@@ -293,7 +178,7 @@ export default Waiting = ({ navigation }) => {
         <View style={styles.playerListRowConatiner} key={i % 2}>
           <View style={styles.leftPlayer} key={i}>
             <Text style={styles.LplayerName} key={i}>
-                {playerList[i]}
+              {playerList[i]}
             </Text>
             <Avatar
                 rounded
@@ -302,13 +187,6 @@ export default Waiting = ({ navigation }) => {
                 }}
                 //icon={{name: 'home'}}
             />
-            {/* <Icon
-                reverse
-                name="sc-telegram"
-                type="evilicon"
-                size={20}
-                color="white"
-            /> */}
           </View>
         </View>
       );
@@ -328,20 +206,20 @@ export default Waiting = ({ navigation }) => {
 
   return (
     <SafeAreaView style={styles.container}>
-        <View style={styles.headerContainer}>
-            <Text style={styles.headerText}>
-                {`${game.hostName}'s Room`}
-                {`\nRoom ID: ${game.gid}`}
-            </Text>
-            <Avatar
-                rounded
-                //style={{alignSelf: "center"}}
-                source={{
-                    uri:"https://images-na.ssl-images-amazon.com/images/S/pv-target-images/7bbe5762c79ee0ad11c1267483b4a2d5e12868de779eaf751e8e86596e978bbb._V_SX1080_.jpg",
-                }}
-                //icon={{name: 'home'}}
-            />
-        </View>
+      <View style={styles.headerContainer}>
+        <Text style={styles.headerText}>
+          {`${game.hostName}'s Room`}
+          {`\nRoom ID: ${game.gid}`}
+        </Text>
+        <Avatar
+          rounded
+          //style={{alignSelf: "center"}}
+          source={{
+              uri:"https://images-na.ssl-images-amazon.com/images/S/pv-target-images/7bbe5762c79ee0ad11c1267483b4a2d5e12868de779eaf751e8e86596e978bbb._V_SX1080_.jpg",
+          }}
+          //icon={{name: 'home'}}
+        />
+      </View>
       <ScrollView style={styles.playersListContainer}>{playerView}</ScrollView>
       <Button
         title={"Confirm"}
@@ -350,11 +228,7 @@ export default Waiting = ({ navigation }) => {
         buttonStyle={{ backgroundColor: color.brown }}
         onPress={() => {
           //wsSend(JSON.stringify({ header: "START", content: gameID }));
-<<<<<<< HEAD:src/components/Waiting.js
           console.log("confirm");
-=======
-          console.log("start"+gameID);
->>>>>>> parent of 876f8ba (.):src/Waiting.js
         }}
       ></Button>
     </SafeAreaView>
@@ -362,8 +236,7 @@ export default Waiting = ({ navigation }) => {
 };
 const styles = StyleSheet.create({
   container: {
-    //backgroundColor: color.offWhite,
-    backgroundColor: color.darkGrey,
+    backgroundColor: color.offWhite,
     flex: 1,
   },
   headerContainer: {
@@ -371,10 +244,10 @@ const styles = StyleSheet.create({
     marginBottom: "20%",
     height: "10%",
     backgroundColor: "#00000080",
-    width: "50%",
+    width: "48%",
     borderTopRightRadius: 50,
     borderBottomRightRadius: 50,
-    //alignContent: "center",
+    alignContent: "center",
     justifyContent: "center",
     flexDirection: "row",
   },
@@ -382,8 +255,8 @@ const styles = StyleSheet.create({
     fontSize: 14,
     fontWeight: "700",
     width:"90%",
-    color: "white",
-    textAlign: "left",
+    color: "#FFFFFFFF",
+    textAlign: "center",
     textAlignVertical: "center",
     paddingLeft:"10%",
   },
@@ -407,9 +280,9 @@ const styles = StyleSheet.create({
   },
   playersListContainer: { height: "60%" },
   button: {
-    height: 60,
+    height: 50,
     width: "50%",
-    marginVertical: 30,
+    marginVertical: 50,
     alignSelf: "center",
     color: color.brown,
     borderRadius: 10,
@@ -426,12 +299,11 @@ const styles = StyleSheet.create({
     height: 100,
     flexDirection: "row",
     justifyContent: "space-between",
-    //padding: 5,
     marginBottom: 5,
   },
   leftPlayer: {
     //flex: 0.36,
-    flexDirection:"row",
+    flexDirection: "row",
     width: "40%",
     height: "100%",
     borderTopRightRadius: 50,
@@ -442,7 +314,7 @@ const styles = StyleSheet.create({
   },
   rightPlayer: {
     //flex: 0.36,
-    flexDirection:"row",
+    flexDirection: "row",
     width: "40%",
     height: "100%",
     borderTopLeftRadius: 50,
